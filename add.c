824912beb55d4bc580f177c77fa6fcf1831db6f0
danc-he <2665669494@qq.com>
2022-03-09 10:08:34 +0800

    cherry-pick finish

--- add.c
+++ add.c
@@ -4,6 +4,10 @@ int main()
 	printf("a + b = %d \n", 1 + 3);
 	printf("add some diff\n");
 	printf("result: %d\n", add(1, 2));
-	//printf("fix bug \n");
+
+	
+
+	printf("fix bug \n");
+	printf("fix bug with cherry-pick.\n");
 	return 0;
 }
