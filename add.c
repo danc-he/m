3596286c1bc1542131798fd6afdd1235d366e23f
danc-he <2665669494@qq.com>
2022-03-09 09:47:53 +0800

    git add . (add add.c add_f.c)

--- add.c
+++ add.c
@@ -3,5 +3,6 @@ int main()
 {
 	printf("a + b = %d \n", 1 + 3);
 	printf("add some diff\n");
+	printf("result: %d\n", add(1, 2));
 	return 0;
 }
--- add_f.c
+++ add_f.c
@@ -1,4 +1,9 @@
 #ifndef __ADD_H__
 #define __ADD_H__
 
+int add (int i, int n)
+{
+	return i + n;
+}
+
 #endif
